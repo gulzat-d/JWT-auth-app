d8a1f6fce978d9bbe44d3ff21165f680b27420dc
gulzat.d <ms.gdlr223@gmail.com>
2023-12-09 13:35:48 +0500

    Change router

--- src/helpers/RequireAuth.tsx
+++ src/helpers/RequireAuth.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from 'react';
+import { ReactNode } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { RootState } from '../store/store';
